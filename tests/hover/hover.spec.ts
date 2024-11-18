725a499931a6dadc3717b54a7960aaf70de645e4
Rajashekar Gunaganti <rajashekarg@a2ailabs.com>
2024-11-18 22:32:39 +0530

    Update hover.spec.ts
    Changes in test spec

--- tests/hover/hover.spec.ts
+++ tests/hover/hover.spec.ts
@@ -1,19 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://www.nseindia.com/');
+  await page.goto('https://www.google.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('NSE - National Stock Exchange of India Ltd: Live Share/Stock Market News & Updates, Quotes- Nseindia.com');
+  await expect(page).toHaveTitle('Google');
 
 });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
